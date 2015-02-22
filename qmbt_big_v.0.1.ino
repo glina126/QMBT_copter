b563c5448dd52db15565f1acad597177378235d5
Damian Glinojecki <glina126@gmail.com>
2015-02-22 14:11:28 -0500

    added a complementary filter and separated the IMU call into its own function

--- qmbt_big_v.0.1.ino
+++ qmbt_big_v.0.1.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <math.h>
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
@@ -25,7 +26,20 @@ Servo motor[4];
 int16_t ax, ay, az; // raw values of the accelerometer output
 int16_t gx, gy, gz; // raw values of the gyroscope output
 
+uint32_t timer; // used to keep track of time
 unsigned long start_time = 0; // used to measure loop cycle time in ms
+
+double raw_x_degree = 0; // raw x angle in degrees
+double raw_y_degree = 0; // raw y angle in degrees
+double raw_z_degree = 0; // raw z angle in degrees
+
+double comp_x_degree = 0; // complimentary x angle in degrees
+double comp_y_degree = 0; // complimentary y angle in degrees
+double comp_z_degree = 0; // complimentary z angle in degrees
+
+double acc_x_degree = 0; // angle estimated using the accelerometer 
+double acc_y_degree = 0; // angle estimated using the accelerometer
+
 
 void setup() {
   // initialize input pins
@@ -62,7 +76,23 @@ void setup() {
   // verify connection
   Serial.println("Testing device connections...");
   Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
+  
+  // should be ran only once and only if needed.
+  //    i.e. if there is too much noise on the acc or gryo
+  // set gyroscope and accelerometer ranges
+  #if 0
+  accelgyro.setFullScaleGyroRange(0x01);
+  accelgyro.setFullScaleAccelRange(0x01); 
+  accelgyro.setDLPFMode(0x06);
+  
+  Serial.print("gyro rate: ");
+  Serial.println(accelgyro.getFullScaleGyroRange());
+  Serial.print("accel rate: ");
+  Serial.println(accelgyro.getFullScaleAccelRange());
+  #endif
+  
+  // initialize the timer 
+  timer = micros(); 
 }
 
 void loop() {  
@@ -88,14 +118,32 @@ void loop() {
     Serial.println(duration[3]);
   #endif
   
+  callIMU();
+  
   // relay the throttle to the motors
   motor[0].writeMicroseconds(duration[3]);
   motor[1].writeMicroseconds(duration[3]);
   motor[2].writeMicroseconds(duration[3]);
   motor[3].writeMicroseconds(duration[3]);
+       
+  // display time per loop cycle 
+  #if 0
+    Serial.print("loop cycle time = ");
+    Serial.println(millis() - start_time);
+  #endif
   
+}
+
+void callIMU()
+{
   // read raw acceletometer and gyro measurements from mpu6050
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  
+  // check how much time has gone by
+  double dt = (double)(micros() - timer) / 1000000; 
+  
+  // update the timer
+  timer = micros(); 
   
   // display raw mpu values
   #if 0
@@ -108,11 +156,57 @@ void loop() {
     Serial.println(gz);
   #endif
   
-  // display time per loop cycle 
+  // calculate the angles 
+  
+  // convert to degrees per second
+  double gyro_x_rate = gx / 131.0; 
+  double gyro_y_rate = gy / 131.0; 
+  double gyro_z_rate = gz / 131.0;
+  
+  // display degrees per second of the xyz axies
   #if 0
-    Serial.print("loop cycle time = ");
-    Serial.println(millis() - start_time);
+    Serial.print("gx/gy/gz in deg/sec = ");
+    Serial.print(gyro_x_rate); Serial.print("\t");
+    Serial.print(gyro_y_rate); Serial.print("\t");
+    Serial.println(gyro_z_rate);
   #endif
   
+  // raw gyro values in degrees
+  raw_x_degree += gyro_x_rate * dt; // Calculate gyro angle without any filter
+  raw_y_degree += gyro_y_rate * dt;
+  raw_z_degree += gyro_z_rate * dt;
+  
+  // display raw degrees of the xyz axies (non-filtered)
+  #if 0
+    Serial.print("gx/gy/gz in deg = ");
+    Serial.print(raw_x_degree); Serial.print("\t");
+    Serial.print(raw_y_degree); Serial.print("\t");
+    Serial.println(raw_z_degree);
+  #endif
+  
+  // convert to angles using the accelerometer  
+  acc_x_degree = atan((double) ay / sqrt((double) ax * (double) ax + (double) az * (double) az)) * RAD_TO_DEG;
+  acc_y_degree = atan2((double) -ax, (double) az) * RAD_TO_DEG;
+  
+  // display angles estimated using the accelerameter
+  #if 0
+    Serial.print("ax/ay in deg = ");
+    Serial.print(acc_x_degree); Serial.print("\t");
+    Serial.println(acc_y_degree);
+  #endif
+  
+  // apply complimentary filtering - kalman is too hefty for this this mcu when using pulseIn :( 
+  comp_x_degree = 0.93 * (comp_x_degree + gyro_x_rate * dt) + 0.07 * acc_x_degree; // Calculate the angle using a Complimentary filter
+  comp_y_degree = 0.93 * (comp_y_degree + gyro_y_rate * dt) + 0.07 * acc_y_degree;
+  comp_z_degree = raw_z_degree; // no magnometer thus we cannot filter the z axis. :(
+  
+  // display angles estimated using the complimentary filter
+  #if 0
+    Serial.print("compl angles x/y/z  = ");
+    Serial.print(comp_x_degree); Serial.print("\t");
+    Serial.print(comp_y_degree); Serial.print("\t");
+    Serial.println(0);
+  #endif
+    
 }
 
